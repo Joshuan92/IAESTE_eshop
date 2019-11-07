43061dc90a5dbee78e3677b4c77a54812c37dd36
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-07 16:47:03 +0100

    migrations

---
+++ database/migrations/2019_11_07_152102_create_local_commitees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocalCommiteesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('local_commitees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('local_commitee_name')->nullable();
+            $table->string('address_street')->nullable();
+            $table->string('address_zip_code')->nullable();
+            $table->string('address_city')->nullable();
+            $table->string('address_country')->nullable();
+            $table->string('ICO')->nullable();
+            $table->string('DICO')->nullable();
+            $table->string('contact_person')->nullable();
+            $table->string('contact_email')->nullable();
+            $table->string('contact_email')->nullable();
+            $table->string('contact_phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('local_commitees');
+    }
+}
