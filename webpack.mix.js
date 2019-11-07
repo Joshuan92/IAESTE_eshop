982e1c562d1f3c520d14c5b412c9eeb5676244c2
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-07 15:14:10 +0100

    React libraries

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,39 @@
 const mix = require('laravel-mix');
-
+require('dotenv').config();
 /*
- |--------------------------------------------------------------------------
+ |-----------------------------------------------------------------------</section><section class="
+ noflag">
  | Mix Asset Management
- |--------------------------------------------------------------------------
+ |-----------------------------------------------------------------------</section><section class="
+ noflag">
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+ 
+mix.options({
+    processCssUrls: false
+});
+ 
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'source-map'
+    })
+    .sourceMaps()
+}
+ 
+mix.react('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+ 
+    .browserSync({
+        host: 'localhost',
+        port: 3000,
+        proxy: {
+            target: process.env.APP_URL // Yay! Using APP_URL from the .env file!
+        }
+    });
+ 
+// add versioning 
+mix.version();
