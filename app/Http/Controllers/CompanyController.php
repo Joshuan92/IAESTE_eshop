21ea4302ed1fc460000bb14bf35efd862f1d3658
Martin Hanzlik <hanzlik.martin.92@gmail.com>
2019-11-09 22:12:27 +0100

    tables companies, users and company_user seeded

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User; 
 
 class CompanyController extends Controller
 {
-    //
+    public function index () {
+
+        $companies = User::orderBy('id')
+                        ->with('company')
+                        ->limit(20)
+                        ->get();
+
+        return $companies;
+
+    }
 }
