ab1cb7dff39f46f3c29c49b1991a452bfb5d53c8
Michal Gazdik <gazdikm@seznam.cz>
2019-11-17 16:53:08 +0100

    fair details

---
+++ resources/js/components/careerFair/FairDetail.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import fairInfo from './fairInfo.js';
+import SingleFair from './SingleFair.jsx';
+import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+
+const FairDetail = (props) => {
+  
+  //const { name, venue, date, img, text } = props
+
+  console.log('props', props)
+  
+  return (
+    <div className="row">
+
+      {/*       <p>{ name }</p>
+            <p>{ text }</p>
+            <p>{ venue }</p>
+            <p>{ date }</p>
+            <p>{ img }</p> */}
+            
+            
+              
+    </div>
+    )
+  } 
+
+export default FairDetail 
