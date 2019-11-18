6c75ac8583343c5bc9303d1e22baa590911b5e55
Michal Gazdik <gazdikm@seznam.cz>
2019-11-18 17:07:31 +0100

    router-detail

--- resources/js/components/careerFair/FairDetail.jsx
+++ resources/js/components/careerFair/FairDetail.jsx
@@ -1,35 +1,28 @@
-import React from 'react';
-import fairInfo from './fairInfo.js';
-import SingleFair from './SingleFair.jsx';
-import { BrowserRouter as Router, Switch, Link } from 'react-router-dom';
+import React from "react";
+import {
 
-const FairDetail = (props) => {
+  useParams,
 
+} from "react-router-dom";
 
-  
-  const { id, name, venue, date, img, text } = props
-
-  console.log('something');
-  
-
-  
-  
-  return (
-    <div className="row">
-
-
-
-            <p>{ id }</p>
-            <p>{ name }</p>
-            <p>{ text }</p>
-            <p>{ venue }</p>
-            <p>{ date }</p>
-            <p>{ img }</p>
+const FairDetail = props => {
+    
+    let { id } = useParams(); // params jsou všechny informace, které přišly na exact path
+    let fair = (props.fairs.filter(f => f.id == id))[0] // mapujeme skrze fairs -–> filtrujeme - filter bere jako parametr funkci a jako argument v našem případě ID. Dvě rovnítka jsou proto, že dostáváme číslo na druhé straně je string.
+    console.log(props.fairs)
+    console.log(id)
+    console.log(fair)
+    const { name, place, event_date, short_description } = fair;
+    return (
+        <div className="row">
             
-            
-              
-    </div>
-    )
-  } 
+            <p>{name}</p>
+            <p>{short_description}</p>
+            <p>{place}</p>
+            <p>{event_date}</p>
+           
+        </div>
+    );
+};
 
-export default FairDetail 
+export default FairDetail;
