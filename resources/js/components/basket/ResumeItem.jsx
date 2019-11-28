79f789148caf8b87cfe4ed72b0a8f7ab368b2019
Michal Gazdik <gazdikm@seznam.cz>
2019-11-28 19:24:17 +0100

    resume form

--- resources/js/components/basket/ResumeItem.jsx
+++ resources/js/components/basket/ResumeItem.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const ResumeItem = (props) => {
-
-    const {name, place, event_date, price} = props;
+const ResumeItem = props => {
+    const { name, place, event_date, price } = props;
 
     return (
         <div className="col-md-4 col-12 mb-4">
-            <div className="card" style={{ width: "18rem", height: "20rem" }}>
+            <div className="card" style={{ width: "10rem", height: "12rem" }}>
                 <div className="card-body">
-                    <h2>{name}</h2>
-                    <h3>{place}</h3>
-                    <h3>{event_date}</h3>
-                    <h3>{price} CZK</h3>
+                    <p>{name}</p>
+                    <p>{place}</p>
+                    <p>{price} CZK</p>
                 </div>
             </div>
         </div>
